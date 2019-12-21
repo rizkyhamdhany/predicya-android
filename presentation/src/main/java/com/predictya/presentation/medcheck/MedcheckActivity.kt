70478b0a87f79b87c72d6d6b85e0800ab8f4ba0a
Rizky Hamdhany <hamdhanywijaya@gmail.com>
2019-12-21 19:18:58 +0700

    all view

---
+++ presentation/src/main/java/com/predictya/presentation/medcheck/MedcheckActivity.kt
@@ -0,0 +1,18 @@
+package com.predictya.presentation.medcheck
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import com.predictya.android.R
+
+class MedcheckActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_medcheck)
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+    }
+}
